fec72cd5c2bd994b46f27751ef86c525b047694a
Bickey Pujari <wakibiki@gmail.com>
2018-05-16 23:45:23 +0545

    add contact and news

--- js/main.js
+++ js/main.js
@@ -16,23 +16,34 @@ $('.video_slider').slick({
   centerPadding: '60px',
   slidesToShow: 3,
   responsive: [
-    {
-      breakpoint: 768,
-      settings: {
-        arrows: true,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 1
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 1
-      }
+  {
+    breakpoint: 1024,
+    settings: {
+      arrows: true,
+      centerMode: true,
+      centerPadding: '40px',
+      slidesToShow: 2
     }
+  },
+  {
+    breakpoint: 768,
+    settings: {
+      arrows: true,
+      centerMode: true,
+      centerPadding: '40px',
+      slidesToShow: 1
+    }
+  },
+  {
+    breakpoint: 480,
+    settings: {
+      arrows: false,
+      centerMode: true,
+      centerPadding: '40px',
+      slidesToShow: 1
+    }
+  }
   ]
 });
+
+$('.matchHeight').matchHeight();
